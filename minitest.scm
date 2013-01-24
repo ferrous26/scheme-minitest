0e7ee6bf2fe5dcaf2ab23f596e22683a49597272
Mark Rada <marada@uwaterloo.ca>
2013-01-24 13:33:29 -0500

    Add assert and refactor assert-equal to use it
    Also refactor so that error messages are more useful.

--- minitest.scm
+++ minitest.scm
@@ -12,14 +12,19 @@
 (define pass-token (lambda () (string-append "\e[" (number->string (next-colour)) "m*\e[0m")))
 (define fail-token "\e[41m\e[37mF\e[0m")
 
-(define assert-equal (lambda (expected actual name)
+(define assert (lambda (expr name msg)
+		 (begin
+		   (set! test-count (+ 1 test-count))
+		   (if (expr)
+		       (display (pass-token))
 		       (begin
-			 (set! test-count (+ 1 test-count))
-			 (if (equal? expected actual)
-			     (display (pass-token))
-			     (begin
-			       (display fail-token)
-			       (set! fails (append fails (list (cons test-count (cons name actual))))))))))
+			 (display fail-token)
+			 (set! fails (append fails (list (cons test-count (cons name msg))))))))))
+
+(define assert-equal (lambda (expected actual name)
+		       (assert (lambda () (equal? expected actual))
+			       name
+			       (list "Expected" actual "to be equal to" expected))))
 
 (define display-test-results (lambda ()
 			       (begin
@@ -41,7 +46,10 @@
 				    (begin
 				      (newline)
 				      (display (string-append (number->string (car fail)) ") "))
-				      (display (string-append (cadr fail) "\n"))
+				      (display (cadr fail))
+				      (newline)
 				      (display (cddr fail))
 				      (newline)))
-				    fails))))
+				    fails)
+
+				 (newline))))
